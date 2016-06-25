14056fd1fa5c3b4f2c36b73f8ee43dd682e64fc7
chrissy <chris@genius.com>
2016-06-25 13:51:36 -0500

    select with envelope

--- server.js
+++ server.js
@@ -11,8 +11,15 @@ const port = 5000;
 var pool = new pg.Pool({database: 'mountains_2'});
 
 app.get('/api', function(request, response) {
+
+  var query = `
+    SELECT name, ref, ST_AsGeoJson(the_geog) AS the_geog
+    FROM osm_trails
+    WHERE ST_Intersects(the_geog, ST_MakeEnvelope(-118.6686774, 36.7933829, -118.5686774, 36.6933829))
+  `
+
   pool.connect(function(err, client, done){
-    client.query('SELECT name, ref, ST_AsGeoJson(the_geog) AS the_geog FROM osm_trails limit 5', function(err, result){
+    client.query(query, function(err, result){
       if (err) throw err;
 
       var features = []
