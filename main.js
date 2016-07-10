4fcf76ea116404ef8b7baa7ddf3f6270c1cca92b
chrissy <chris@genius.com>
2016-07-10 12:57:31 -0500

    don't use react-mapbox-gl because it is literally terrible

--- main.js
+++ main.js
@@ -1,45 +1,26 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var MapGL = require('react-map-gl');
-var window = require('global/window');
-var document = require('global/document');
+var MapboxGL = require('mapbox-gl');
+
+MapboxGL.accessToken = 'pk.eyJ1IjoiZml2ZWZvdXJ0aHMiLCJhIjoiY2lvMXM5MG45MWFhenUybTNkYzB1bzJ0MiJ9._5Rx_YN9mGwR8dwEB9D2mg'
 
 var Map = React.createClass({
   getInitialState: function() {
-    return {
-      viewport: {
-        width: window.innerWidth,
-        height: window.innerHeight,
-        latitude: 36.7933829,
-        longitude: -118.6686774,
-        mapStyle: 'mapbox://styles/mapbox/outdoors-v9',
-        zoom: 10,
-        isDragging: false,
-        mapboxApiAccessToken: 'pk.eyJ1IjoiZml2ZWZvdXJ0aHMiLCJhIjoiY2lvMXM5MG45MWFhenUybTNkYzB1bzJ0MiJ9._5Rx_YN9mGwR8dwEB9D2mg'
-      }
-    }
+    return {}
   },
 
   componentDidMount: function() {
-    window.addEventListener('resize', function() {
-      this.setState({
-        viewport: Object.assign({}, this.state.viewport, {
-          width: window.innerWidth,
-          height: window.innerHeight
-        })
-      });
-    }.bind(this));
-  },
-
-  _onChangeViewport: function(viewport) {
-    this.setState({
-      viewport: Object.assign({}, this.state.viewport, viewport)
-    })
+    var map = new MapboxGL.Map({
+      container: 'mapbox-gl-element',
+      style: 'mapbox://styles/mapbox/outdoors-v9',
+      center: [-118.6686774, 36.7933829],
+      zoom: 10
+    });
   },
 
   render: function() {
     return (
-      <MapGL {...Object.assign({}, this.state.viewport, {onChangeViewport: this._onChangeViewport})} />
+      <div id="mapbox-gl-element"></div>
     );
   }
 })
