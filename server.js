bab104e9a65e40290bf40f258f01da21b5c46e1d
chrissy <chris@genius.com>
2016-09-11 15:05:09 -0500

    messy but working internal altitude api

--- server.js
+++ server.js
@@ -74,6 +74,43 @@ app.get('/api/trails/:id', function(request, response) {
   })
 })
 
+app.get('/api/elevation/:id', function(request, response){
+  let query = `
+    select ST_AsGeoJson(the_geog) as the_geog
+    FROM osm_trails
+    WHERE ogc_fid = ${request.params.id}
+  `
+
+  pool.connect(function(err, client, done){
+    client.query(query, function(err, result){
+      if (err) throw err;
+
+      let r = result.rows[0];
+      let points = JSON.parse(r.the_geog).coordinates;
+      const altitudes = []
+      points.forEach(function(point, i) {
+        let query = `
+          SELECT ST_Value(rast, ST_Transform(
+            ST_GeomFromText(
+              'POINT(${point[0]} ${point[1]})',
+            4326), 4326)
+          )
+          FROM elevation_GMTED2010N30W120_150
+          WHERE rid=4
+        `;
+        client.query(query, function(err, result){
+          if (err) throw err;
+          if (result) altitudes.push(result.rows[0].st_value)
+          if (i + 1 >= points.length) {
+            done();
+            response.json(altitudes);
+          }
+        })
+      });
+    });
+  });
+});
+
 app.listen(5000, function () {
   console.log('listening on port 5000');
 });
