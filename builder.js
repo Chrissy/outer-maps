f6e292bf85762d7f560d2112ad444ba3bfff7079
chrissy <chris@genius.com>
2017-05-05 12:41:45 -0500

    asynchonously build trail files and upload them

--- builder.js
+++ builder.js
@@ -1,24 +1,43 @@
 const fs = require('fs');
 const jsonfile = require('jsonfile')
 const helpers = require('@turf/helpers');
-const execSync = require('child_process').execSync;
+const exec = require('child_process').spawn;
 const path = require('path').normalize;
 const gQuery = require('./modules/genericQuery');
 
 const pool = gQuery.pool();
 
 build = (name, result) => {
-  console.log('building...');
+  const tempFileName = `map_dist/${name}.geojson`;
+  console.log(`building ${name}...`);
+
   gQuery.geoJson(result, (result) => {
-    jsonfile.writeFileSync(`_temp_${name}.geojson`, result);
-    execSync(`mapbox upload ${name} _temp_${name}.geojson`, {stdio:[0,1,2]});
-    fs.unlink(`_temp_${name}.geojson`);
+    jsonfile.writeFile(tempFileName, result, () => {
+      const proc = exec('mapbox', ['upload', name, tempFileName]);
+      proc.on('close', () => console.log(`${name} uploaded!`));
+    });
   });
 };
 
 gQuery.query(`
-  SELECT
-    name, id, type, ST_SimplifyVW(geog::geometry, 0.0000005) as geom
+  SELECT name, id, type, ST_SimplifyVW(geog::geometry, 0.0000005) as geom
   FROM trails
   WHERE type = 'hike' OR type = 'horse' AND name != ''
 `, pool, (result) => build("trails-hike-horse", result));
+
+gQuery.query(`
+  SELECT name, id, type, ST_SimplifyVW(geog::geometry, 0.0000005) as geom
+  FROM trails
+  WHERE type = 'bike' AND name != ''
+`, pool, (result) => build("trails-bike-hike", result));
+
+gQuery.query(`
+  SELECT name, id, type, ST_SimplifyVW(geog::geometry, 0.0000005) as geom
+  FROM trails
+  WHERE type = 'atv' OR type = 'motorcycle' AND name != ''
+`, pool, (result) => build("trails-atv-motorcycle", result));
+
+gQuery.query(`
+  SELECT name, id, ST_Simplify(geog::geometry, 0.0005) as geom
+  FROM boundaries
+`, pool, (result) => build("park-boundaries", result));
