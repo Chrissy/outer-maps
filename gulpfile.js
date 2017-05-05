34060fc0f48150307160a6a7132d9cc2d0151cdd
chrissy <chris@genius.com>
2017-05-05 16:56:25 -0500

    use gulp for great good. break this insane mapbox style file into files i can actually digest, and of course reconstruct them using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,16 @@ const gulp = require('gulp');
 const merge = require('gulp-merge-json');
 
 gulp.task('mapbox-styles', function(){
-  return gulp.src('styles/*.json')
+  return gulp.src([
+      'styles/base.json',
+      'styles/metadata.json',
+      'styles/landcover.json',
+      'styles/elevation.json',
+      'styles/roads.json'
+    ])
     .pipe(merge({
-      fileName: 'mbstyles.json'
+      fileName: 'mbstyles.json',
+      concatArrays: true
     }))
     .pipe(gulp.dest('public'))
 });
