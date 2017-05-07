e8cd883e7829542be162f18d21b3468c08f2faf4
chrissy <chris@genius.com>
2017-05-07 12:04:32 -0500

    use gulp plumber to keep the party going when json errors occur

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const gulp = require('gulp');
 const merge = require('gulp-merge-json');
+const plumber = require('gulp-plumber');
 
 gulp.task('mapify', () => {
   var index = JSON.parse(fs.readFileSync('./styles/index.json'));
   return gulp.src(index.files)
+  .pipe(plumber())
   .pipe(merge({
     fileName: 'mapbox-styles.json',
     concatArrays: true
   }))
+  .on('error', (err) => console.log(err))
   .pipe(gulp.dest('public/dist'))
 });
 
