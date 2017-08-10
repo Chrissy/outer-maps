05a10bcd6ca2963b86b417ad710f15f97ad32f01
chrissy <chris@genius.com>
2017-08-09 17:54:24 -0700

    🎉 🎉 🎉 🎉  WEBPACK! 🎉 🎉 🎉 🎉

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './components/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["env", "es2015", "react"],
+            plugins: ["transform-object-rest-spread"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+      }
+    ]
+  }
+};
