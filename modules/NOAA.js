078b5fced3e704feaf9dd8965ed3afeb15862dd9
chrissy <chris@genius.com>
2017-08-11 12:47:00 -0700

    recursive station getting up to .6 miles

--- modules/NOAA.js
+++ modules/NOAA.js
@@ -3,38 +3,45 @@ import distance from '@turf/distance';
 const moment = new Moment;
 const key = 'aMjnYsZqxVmjzkbPYtHBVXUnYHUROvwS';
 
-const get = function(path) {
+const get = path => {
   return fetch(`https://www.ncdc.noaa.gov/cdo-web/api/v2${path}`, {
     headers: new Headers({'token': key})
-  }).then(response => response.json())
-  .then(data => data);
-}
+  }).then(response => response.json());
+};
 
-const getData = function({dataSetID, stationID, date, endDate, dataTypeIDs}) {
-  return new Promise(function(resolve){
-    get(`/data?datasetid=${dataSetID}&stationid=${stationID}&startdate=2010-${date}&enddate=2010-${date}&datatypeid=${dataTypeIDs.join("&datatypeid=")}&limit=10&units=standard`).then(({results}) => {
-      resolve(results.reduce((r, v) => {
-        return {...r, [v.datatype]: v}
-      }, {}));
+const getData = ({dataSetID, stationID, date, endDate, dataTypeIDs}) => {
+  return get(`/data?datasetid=${dataSetID}&stationid=${stationID}&startdate=2010-${date}&enddate=2010-${date}&datatypeid=${dataTypeIDs.join("&datatypeid=")}&limit=10&units=standard`).then(({results}) => {
+    return results.reduce((r, v) => {
+      return {...r, [v.datatype]: v}
+    }, {});
+  });
+};
+
+const getStations = ({x, y, dataSetID, size = 0.1,}) => {
+  return new Promise(resolve => {
+    get(`/stations/?extent=${x - size},${y - size},${x + size},${y + size}&datasetid=${dataSetID}`).then(response => {
+      if (response.results) {
+        resolve(response.results);
+      } else if (size > 0.6) {
+        resolve(null);
+      } else {
+        resolve(getStations({x, y, dataSetID, size: size + 0.1}));
+      }
     });
   });
-}
-
-const getStation = function({x, y, dataSetID, size = 0.3}) {
-  return get(`/stations/?extent=${x - size},${y - size},${x + size},${y + size}&datasetid=${dataSetID}`).then(function(response){
-    return response.results.sort(function(a, b) {
-      return distance([a.x, a.y], [x, y]) - distance([b.x, b.y], [x, y]);
-    })[0];
-  });
-}
+};
 
 export default ({x, y, dataSetID, dataTypeIDs}) => {
-  return getStation({x, y, dataSetID}).then(station => {
-    if (station) return getData({
+  return getStations({x, y, dataSetID}).then(stations => {
+    if (!stations.length) return null;
+    const station = stations.sort((a, b) => {
+      return distance([a.x, a.y], [x, y]) - distance([b.x, b.y], [x, y]);
+    })[0];
+    return getData({
       stationID: station.id,
       date: moment.format("MM-DD"),
       dataSetID,
       dataTypeIDs,
     });
   });
-}
+};
