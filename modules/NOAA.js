57b1bd87001258c0a7d6baa263d866347eba106c
chrissy <chris@genius.com>
2017-08-10 17:16:14 -0700

    remove a bunch of junk that wasn't being used

--- modules/NOAA.js
+++ modules/NOAA.js
@@ -2,7 +2,7 @@ import Moment from 'moment';
 
 const key = 'aMjnYsZqxVmjzkbPYtHBVXUnYHUROvwS';
 
-const getData = function({dataSetID = null, stationID = null, startDate="01-01", endDate="01-01", dataTypeIDs = {}} = {}) {
+const getData = function({dataSetID, stationID, startDate="01-01", endDate="01-01", dataTypeIDs = {}} = {}) {
   return new Promise(function(resolve){
     get(`/data?datasetid=${dataSetID}&stationid=${stationID}&startdate=2010-${startDate}&enddate=2010-${endDate}&datatypeid=${Object.values(dataTypeIDs).join("&datatypeid=")}&limit=10&units=standard`).then((response) => {
       let newObj = {};
@@ -14,13 +14,13 @@ const getData = function({dataSetID = null, stationID = null, startDate="01-01",
   });
 }
 
-const getDataForToday = function({dataSetID = null, stationID = null, dataTypeIDs = {}} = {}) {
+const getDataForToday = function({dataSetID, stationID, dataTypeIDs = {}} = {}) {
   const moment = new Moment;
   const dateString = moment.format("MM-DD");
   return getData({startDate: dateString, endDate: dateString, dataSetID: dataSetID, stationID: stationID, dataTypeIDs: dataTypeIDs})
 }
 
-const getStation = function({x = null, y = null, dataSetID = null, size = 0.3}) {
+const getStation = function({x, y, dataSetID, size = 0.3}) {
   return get(`/stations/?extent=${x - size},${y - size},${x + size},${y + size}&datasetid=${dataSetID}`).then(function(response){
     return response.results.sort(function(a, b) {
       return distance(a, {x: x, y: y}) - distance(b, {x: x, y: y});
@@ -28,8 +28,8 @@ const getStation = function({x = null, y = null, dataSetID = null, size = 0.3}) 
   });
 }
 
-const getDataFromNearestStation = function({x = null, y = null, dataSetID = "", dataTypeIDs = {}} = {}) {
-  return getStation({x: x, y: y, dataSetID: dataSetID}).then(function(station) {
+const getDataFromNearestStation = function({x, y, dataSetID = "", dataTypeIDs = {}} = {}) {
+  return getStation({x: x, y: y, dataSetID: dataSetID}).then(station => {
     if (station) {
       return getDataForToday({stationID: station.id, dataSetID: dataSetID, dataTypeIDs: dataTypeIDs});
     }
@@ -49,26 +49,6 @@ const findByDatatype = (datatype, data) => {
 
 const convertToPercent = (integer) => parseInt(integer/10);
 
-const printDataSetsForStation = (stationID) => get(`/datasets?stationid=${stationID}`).then(r => print(r.results));
-
-const getDataTypesForDataCategory = (dataCategoryID) => get(`/datatypes?datacategoryid=${dataCategoryID}`);
-
-const getDataTypesForStationAndDataset = (stationID, dataSetID) => get(`/datatypes?stationid=${stationID}&datasetid=${dataSetID}&limit=100`);
-
-const getDataCategories = () => get(`/datacategories?limit=50`);
-
-const printDataSets = () => get("/datasets").then(r => print(r.results));
-
-const printDataSetInfo = (dataSet) => get(`/datasets/${dataSet}`).then(r => console.log(r));
-
-const getDataTypeInfo = (dataTypeId) => get(`/datatypes/${dataTypeId}`);
-
-const getLocationCategories = () => get("/locationcategories");
-
-const distance = function(point1, point2) {
-  return Math.abs(point1.x - point2.x) + Math.abs(point1.y - point2.y);
-}
-
-const print = (results) => results.forEach(r => console.log(r.name, r.id));
+const distance = (point1, point2) => Math.abs(point1.x - point2.x) + Math.abs(point1.y - point2.y);
 
 export {getDataFromNearestStation, convertToPercent}
