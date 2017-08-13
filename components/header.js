f409b12be6f08e9ce2ae665a20515661d61fa12a
chrissy <chris@genius.com>
2017-08-13 15:26:15 -0700

    header component and svg webpack duder

---
+++ components/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = (props) => {
+  render() {
+    return (
+      <div class="header">
+    )
+  }
+};
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -26,6 +26,10 @@ module.exports = {
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
       }
     ]
   }
