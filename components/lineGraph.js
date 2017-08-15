78e6bee78852db7f0ed3e79707929187f78e9624
chrissy <chris@genius.com>
2017-08-15 00:09:44 -0700

    mileage markers for the graph

--- components/lineGraph.js
+++ components/lineGraph.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import _ from 'underscore';
 import styles from '../styles/lineGraph.css';
+import {metersToMiles} from '../modules/conversions'
 
 export default class LineGraph extends React.Component {
 
-  pointsToPathString() {
+  distances() {
     var points = this.props.elevations;
-    var elevations = points.map(e => e.elevation);
-    var distances = points.map(p => p.distanceFromPreviousPoint).reduce((a, p) => a.concat(p + _.last(a) || 0), []);
-    var maxElevation = Math.max(...elevations);
-    var elevationWindow = maxElevation - Math.min(...elevations);
-    var fullDistance = _.last(distances);
-    var relativePoints = elevations.map((elevation, i) => [((maxElevation - elevation)/elevationWindow), (distances[i]/fullDistance)]);
+    return points.map(p => p.distanceFromPreviousPoint).reduce((a, p) => a.concat(p + _.last(a) || 0), []);
+  }
+
+  pointsToPathString() {
+    const elevations = this.props.elevations.map(e => e.elevation);
+    const maxElevation = Math.max(...elevations);
+    const elevationWindow = maxElevation - Math.min(...elevations);
+    const distances = this.distances();
+    const fullDistance = _.last(distances);
+    const relativePoints = elevations.map((elevation, i) => [((maxElevation - elevation)/elevationWindow), (distances[i]/fullDistance)]);
 
     return relativePoints.reduce((a,p,i) => a + `${p[1] * 250},${p[0] * 100} `, "0,100 ") + "250,100";
   }
 
+  mileMarkers(){
+    const miles = metersToMiles(_.last(this.distances()));
+    const iterations = Math.ceil(miles);
+    const width = parseInt(250 / miles) + ((250 % miles) / miles);
+    const leftOver = width * iterations - 250;
+    let markers = [];
+    for (let step = 0; step < iterations; step++) {
+      markers.push(<rect
+        x={width * step}
+        width={(step == iterations - 1) ? width - leftOver : width}
+        height={100}
+        key={step}
+        fill={(step % 2 !== 0) ? "transparent" : "#EAEAEA"}
+      />);
+    }
+    return markers;
+  }
+
   render() {
     return (
-      <svg className={styles.lineGraph} viewBox="0 0 250 100">
+      <svg className={styles.lineGraph} viewBox="0 0 250 100" overflow="hidden">
+        <g>{this.mileMarkers()}</g>
         <polyline points={this.pointsToPathString()} strokeWidth="2" fill="#344632"/>
       </svg>
     )
