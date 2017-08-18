ef32501772ddb45c0574782acc500a5f76df05b6
chrissy <chris@genius.com>
2017-08-17 17:20:13 -0700

    completely remove env because hghrhhaaaahh

--- modules/mapboxStyleLoader.js
+++ modules/mapboxStyleLoader.js
@@ -1,13 +1,12 @@
 const req = require.context("../styles", true, /\.json$/);
 const base = req("./base.json");
-//const env = require("../environment/development.js");
 const baseLayers = base.layers.map(l => req(`./${l}.json`));
-const source = (process.env.NPM_CONFIG_PRODUCTION || env.tileSource == "remote") ? "composite" : "local"
+const source = "composite"
 
 export default Object.assign({}, base, {
   layers: [].concat(...baseLayers).map(l => {
     if (l.source && l.source == "$source") l.source = source;
     return l;
   }),
-  sources: (process.env.NPM_CONFIG_PRODUCTION) ? "remote" : base[env.tileSource]
+  sources: base["remote"]
 });
