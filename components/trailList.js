bc5136d2bb746e4bbb2172a490978b0664781592
chrissy <chris@genius.com>
2017-08-20 09:49:47 -0700

    add mileage

--- components/trailList.js
+++ components/trailList.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {metersToMiles} from '../modules/conversions';
+
 
 export default class TrailList extends React.Component {
+  listElement(trail) {
+    return <div>{trail.name} | {metersToMiles(trail.distance)}</div>
+  }
+
   render() {
     return (
-      <div>{this.props.trails.map(t => t.name).join(" ")}</div>
+      <div>{this.props.trails.map(t => this.listElement(t))}</div>
     )
   }
 };
