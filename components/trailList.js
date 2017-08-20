1d2d61dfdc05ca7fcd8351fc2f1e639a617872e5
chrissy <chris@genius.com>
2017-08-20 10:10:59 -0700

    trail list styles

--- components/trailList.js
+++ components/trailList.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import {metersToMiles} from '../modules/conversions';
-
+import cx from 'classnames';
+import styles from '../styles/trailList.css';
+import spacing from '../styles/spacing.css';
 
 export default class TrailList extends React.Component {
   listElement(trail) {
-    return <div>{trail.name} | {metersToMiles(trail.distance)}</div>
+    return (
+      <div className={cx(styles.listElement, spacing.marginBottomHalf)} key={trail.id}>
+        <div>{trail.name}</div>
+        <div>{metersToMiles(trail.distance)}</div>
+      </div>
+    )
   }
 
   render() {
     return (
-      <div>{this.props.trails.map(t => this.listElement(t))}</div>
+      <div className={cx(styles.trailList, spacing.marginBottomTriple, spacing.marginTopHalf, spacing.horizontalPadding)}>
+        {this.props.trails.map(t => this.listElement(t))}
+      </div>
     )
   }
 };
