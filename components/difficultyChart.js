545544d227ede9981537d0ecaf117fd331553a85
chrissy <chris@genius.com>
2017-09-02 09:50:56 -0700

    pure function difficultyChart with proptypes

--- components/difficultyChart.js
+++ components/difficultyChart.js
@@ -1,40 +1,45 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import styles from '../styles/difficultyChart.css';
 import center from '../styles/center.css';
 import label from '../styles/label.css';
 import cx from 'classnames';
 
-export default class DifficultyChart extends React.Component {
+const DifficultyChart = ({score}) => {
 
-  rotation() {
-    return ((this.props.score > 50) ? 225 : 45) + (((this.props.score - 50) / 100) * 360);
+  const rotation = () => {
+    return ((score > 50) ? 225 : 45) + (((score - 50) / 100) * 360);
   }
 
-  difficulty() {
-    if (this.props.score < 25) return 'Very Easy';
-    if (this.props.score < 50) return 'Easy';
-    if (this.props.score < 75) return 'Moderate';
-    if (this.props.score < 96) return 'Strenuous';
-    if (this.props.score >= 96) return 'Extreme';
+  const difficulty = () => {
+    if (score < 25) return 'Very Easy';
+    if (score < 50) return 'Easy';
+    if (score < 75) return 'Moderate';
+    if (score < 96) return 'Strenuous';
+    if (score >= 96) return 'Extreme';
   }
 
-  difficultyClass() {
-    return this.difficulty().replace(/\s+/g, '').toLowerCase();
+  const difficultyClass = () => {
+    return difficulty().replace(/\s+/g, '').toLowerCase();
   }
 
-  render() {
-    return (
-      <div className={cx(styles.body, center.flex, styles[this.difficultyClass()], {[styles.halfEmpty]: this.props.score < 50})}>
-        <div className={styles.circle1}></div>
-        <div className={styles.circle2} style={{transform: `rotate(${this.rotation()}deg)`}}></div>
-        <div className={cx(styles.innerCircle, center.flex)}>
-          <div className={styles.data}>
-            <div className={cx(styles.label, label.label)}>Difficulty</div>
-            <div className={styles.score}>{this.props.score}</div>
-            <div className={styles.difficulty}>{this.difficulty()}</div>
-          </div>
+  return (
+    <div className={cx(styles.body, center.flex, styles[difficultyClass()], {[styles.halfEmpty]: score < 50})}>
+      <div className={styles.circle1}></div>
+      <div className={styles.circle2} style={{transform: `rotate(${rotation()}deg)`}}></div>
+      <div className={cx(styles.innerCircle, center.flex)}>
+        <div className={styles.data}>
+          <div className={cx(styles.label, label.label)}>Difficulty</div>
+          <div className={styles.score}>{score}</div>
+          <div className={styles.difficulty}>{difficulty()}</div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 };
+
+DifficultyChart.propTypes = {
+  score: PropTypes.number
+}
+
+export default DifficultyChart;
