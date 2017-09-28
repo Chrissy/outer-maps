135334ebac10737d7669d7f0b673d80b54d637e4
chrissy <chris@genius.com>
2017-09-28 16:45:34 -0700

    get the standalone mercator flat viewport thingy from the package so that we don't import all the 3d stuff

--- components/terrain.js
+++ components/terrain.js
@@ -4,7 +4,7 @@ import styles from '../styles/terrain.css';
 import center from '../styles/center.css';
 import cx from 'classnames';
 import LoadingSpinner from './loadingSpinner';
-import {FlatMercatorViewport} from 'viewport-mercator-project';
+import FlatMercatorViewport from '../node_modules/viewport-mercator-project/src/flat-mercator-viewport';
 import pin from '../modules/pin';
 
 export default class Terrain extends React.Component {
