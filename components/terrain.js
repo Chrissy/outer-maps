35289b6e9d401264c918b525532a4e56e7ad5d0e
chrissy <chris@genius.com>
2017-09-27 23:49:02 -0700

    working coordinate helper thingys

--- components/terrain.js
+++ components/terrain.js
@@ -8,6 +8,7 @@ import {FlatMercatorViewport} from 'viewport-mercator-project';
 import GeoViewport from '@mapbox/geo-viewport';
 import pin from '../modules/pin';
 import arrayEquals from '../modules/arrayEquals';
+import Triangle from '../svg/triangle.svg';
 
 export default class Terrain extends React.Component {
 
@@ -18,6 +19,15 @@ export default class Terrain extends React.Component {
     return this.props.points.map(p => {
       return {...p, coordinates: projecter.project(p.coordinates)}
     });
+  }
+
+  pointCloseToEdge(point, buffer) {
+    const canvas = this.refs.canvas;
+
+    if (point[0] < buffer) return 'left';
+    if (point[1] < buffer) return 'top';
+    if (point[0] > canvas.width - buffer) return 'right';
+    if (point[1] > canvas.height - buffer) return 'bottom';
   }
 
   drawPath(points) {
@@ -53,9 +63,31 @@ export default class Terrain extends React.Component {
     return (this.props.satelliteImageUrl);
   }
 
+  latLongElement(point, index) {
+    const [x, y] = point.coordinates;
+    return <div
+      className={cx(styles.latLong, styles[this.pointCloseToEdge(point, 20)])}
+      style={{left: (x / 1024) * 100 + '%', top: (y / 1024) * 100 + '%'}}>
+        {this.props.points[index].coordinates[0].toString().slice(0, 7)},&nbsp;
+        {this.props.points[index].coordinates[1].toString().slice(0, 5)}
+        <Triangle className={cx(styles.triangle)} />
+      </div>
+  }
+
+  latLongElementFirst() {
+    if (!this.projectedPoints || !this.projectedPoints.length || !this.props.points) return '';
+    return this.latLongElement(this.projectedPoints[0], 0);
+  }
+
+  latLongElementSecond() {
+    if (!this.projectedPoints || !this.projectedPoints.length || !this.props.points) return '';
+    return this.latLongElement(this.projectedPoints[this.projectedPoints.length - 1], this.projectedPoints.length - 1);
+  }
+
   componentDidUpdate(prevProps) {
     if (!prevProps.points || !this.props.points || !arrayEquals(prevProps.points, this.props.points)) {
-      this.drawPath(this.projectPoints(this.props));
+      this.projectedPoints = this.projectPoints(this.props);
+      this.drawPath(this.projectedPoints);
     }
   }
 
@@ -65,6 +97,8 @@ export default class Terrain extends React.Component {
         <img src={this.props.satelliteImageUrl} className={cx(styles.image, {[styles.visible]: this.isVisible()})}/>
         <canvas ref="canvas" width="1026" height="1026" className={cx(styles.canvas, {[styles.visible]: this.isVisible()})}></canvas>
         <div className={cx(styles.loadingSpinner, {[styles.visible]: !this.isVisible()})}><LoadingSpinner speed="1s"/></div>
+        {this.latLongElementFirst()}
+        {this.latLongElementSecond()}
       </div>
     )
   }
