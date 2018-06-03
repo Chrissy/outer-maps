ef3b14ad68a8934e626283ca5d694fcbaec24c6a
Chrissy <christopherchristopherclark@gmail.com>
2018-06-03 11:00:19 -0700

    setup jest and add first test

--- modules/instagramStaticData.js
+++
@@ -1,6 +0,0 @@
-{
-  accessToken: '1276647.dc9f869.dbabce789d154c1ab3268d568b0a5907',
-  haxessToken: '1276647.e029fea.b609262697144f4c8a5369f75b0cab69',
-  haxorId: '1276647',
-  exampleUrl: 'https://api.instagram.com/v1/locations/search?lat=36.7933829&lng=-118.6686774&access_token=1276647.dc9f869.dbabce789d154c1ab3268d568b0a5907'
-}
