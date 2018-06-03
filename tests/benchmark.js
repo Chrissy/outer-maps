77391aa3a443a0e7f8bbcc134bb4c93699048f17
Chrissy <christopherchristopherclark@gmail.com>
2018-06-03 16:07:22 -0700

    actually working benchmark tests

--- tests/benchmark.js
+++ tests/benchmark.js
@@ -1,15 +1,33 @@
 const Benchmark = require('benchmark');
 const getTrail = require('../modules/getTrail');
+const getBoundary = require('../modules/getBoundary');
 const createPool = require('../modules/genericQuery').pool;
 
 const pool = createPool();
 const suite = new Benchmark.Suite;
 
-suite.add("get trail", async () => {
-  await getTrail(141044, pool)
-}).on('cycle', function(event) {
+suite
+.add("get trail", {
+  fn: async (deferred) => {
+    //current benchmark ~1.2ops/sec
+    await getTrail(141044, pool);
+    deferred.resolve();
+  },
+  defer: true
+})
+.add("get boundary", {
+  fn: async (deferred) => {
+    //current benchmark ~1.1ops/sec
+    await getBoundary(1496, pool);
+    deferred.resolve();
+  },
+  defer: true
+})
+.on('cycle', function(event) {
   console.log(String(event.target));
-}).on('complete', async () => {
-  console.log("benchmark tests complete")
+})
+.on('complete', async () => {
+  console.log("tests complete")
   await pool.end();
-}).run({'async': true})
+})
+.run({'async': true})
