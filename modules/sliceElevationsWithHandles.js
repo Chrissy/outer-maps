101fd0a903dee0381c858e92fb2056d4973d18ab
Chrissy <christopherchristopherclark@gmail.com>
2018-06-03 22:15:33 -0700

    test for sliceElevationsWithHandles

--- modules/sliceElevationsWithHandles.js
+++ modules/sliceElevationsWithHandles.js
@@ -1,8 +1,8 @@
 export default (trail, handles) => {
-  const hs = handles.filter(h => h.trailId == trail.id);
-  if (hs.length < 2) return trail;
+  const matchingHandles = handles.filter(h => h.trailId == trail.id);
+  if (matchingHandles.length < 2) return trail;
 
-  const indeces = hs.map(h => h.index).sort((a,b) => a - b);
+  const indeces = matchingHandles.map(h => h.index).sort((a,b) => a - b);
   return Object.assign({}, trail, {
     points: trail.points.slice(indeces[0], indeces[1])
   });
