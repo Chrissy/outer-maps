e7e6dfe200140cd4bb90020368ae8c5e59a899d3
Chrissy <christopherchristopherclark@gmail.com>
2018-07-07 21:14:55 -0700

    timers for some production benchmarking

--- services/fetchWithCache.js
+++ services/fetchWithCache.js
@@ -7,16 +7,20 @@ const fetchWithFailure = path =>
 
 const tryCachedFile = ({ path, extension }) =>
   new Promise((resolve, reject) => {
+    console.time(`caching at ${path}`) //this will be removed soon;
+
     const cachedPath = `https://s3-us-west-2.amazonaws.com/chrissy-gunk/${path
       .slice(5)
       .replace(/\//g, "-")}.${extension}`;
 
     fetchWithFailure(cachedPath).then(response => {
       if (!response.failure) {
+        console.timeEnd(`caching at ${path}`) //this will be removed soon;
         return resolve(response);
       } else {
         fetchWithFailure(path).then(response => {
           if (!response.failure) {
+
             return resolve(response);
           } else {
             reject("Rejected by both the cache and the api");
