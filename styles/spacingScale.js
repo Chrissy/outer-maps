8efa97f35440710ff76bba46f25d736e75f3961b
Chrissy <christopherchristopherclark@gmail.com>
2018-09-22 14:35:36 -0700

    set up react-emotion with theming

---
+++ styles/spacingScale.js
@@ -0,0 +1,34 @@
+export default size => {
+  switch (size) {
+  case 1:
+    return "0.125em";
+  case 2:
+    return "0.25em";
+  case 3:
+    return "0.5em";
+  case 4:
+    return "0.75em";
+  case 5:
+    return "1em";
+  case 6:
+    return "1.125em";
+  case 7:
+    return "1.25em";
+  case 8:
+    return "1.5em";
+  case 9:
+    return "2em";
+  case 10:
+    return "2.5em";
+  case 11:
+    return "3em";
+  case 12:
+    return "4em";
+  case 13:
+    return "5em";
+  case 14:
+    return "10em";
+  case 15:
+    return "20em";
+  }
+};
---
+++ styles/typeScale.js
@@ -0,0 +1,34 @@
+export default size => {
+  switch (size) {
+  case 1:
+    return "0.5em";
+  case 2:
+    return "0.75em";
+  case 3:
+    return "0.875em";
+  case 4:
+    return "1em";
+  case 5:
+    return "1.125em";
+  case 6:
+    return "1.25em";
+  case 7:
+    return "1.35em";
+  case 8:
+    return "1.5em";
+  case 9:
+    return "1.66em";
+  case 10:
+    return "1.75em";
+  case 11:
+    return "1.875em";
+  case 12:
+    return "2em";
+  case 13:
+    return "2.5em";
+  case 14:
+    return "3em";
+  case 15:
+    return "4em";
+  }
+};
